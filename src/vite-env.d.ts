c61fb4cf56c4747bfe763f0bca62a709f548f8af
paulobmcosta-creator <paulobmcosta@gmail.com>
2026-09-17 11:44:53 -0300

    chore: remove obsolete AI Studio preview global

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,7 +1,5 @@
 /// <reference types="vite/client" />
 
-declare const __ODC_AI_STUDIO_PREVIEW__: boolean;
-
 interface ImportMetaEnv {
   readonly VITE_API_BASE_URL?: string;
   readonly VITE_FIREBASE_API_KEY?: string;
